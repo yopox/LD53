572efa02f401586bfcbaea6b609712142caec62a
yopox <yopoxdev@gmail.com>
2023-04-29 19:48:41 +0200

    grid: Update aesthetic

--- src/graphics/grid.rs
+++ src/graphics/grid.rs
@@ -2,12 +2,11 @@ use std::cmp::{max, min};
 
 use bevy::prelude::*;
 use enum_derived::Rand;
+use rand::RngCore;
 
 use crate::{GameState, logic, util};
+use crate::graphics::{sprite, sprites};
 use crate::graphics::loading::Textures;
-use crate::graphics::palette::Palette;
-use crate::graphics::sprite;
-use crate::graphics::sprites::TILE;
 
 pub struct GridPlugin;
 
@@ -35,136 +34,98 @@ fn setup(
     textures: Res<Textures>,
 ) {
     let points = vec![
-        Vec2::new(0., 7.),
-        Vec2::new(3., 7.),
-        Vec2::new(3., 3.),
-        Vec2::new(8., 3.),
-        Vec2::new(8., 7.),
-        Vec2::new(11., 7.),
-        Vec2::new(11., 3.),
-        Vec2::new(16., 3.),
-        Vec2::new(16., 7.),
-        Vec2::new(20., 7.),
+        Vec2::new(0., 6.),
+        Vec2::new(3., 6.),
+        Vec2::new(3., 4.),
+        Vec2::new(8., 4.),
+        Vec2::new(8., 6.),
+        Vec2::new(11., 6.),
+        Vec2::new(11., 4.),
+        Vec2::new(16., 4.),
+        Vec2::new(16., 6.),
+        Vec2::new(20., 6.),
     ];
     let path = logic::path::Path::from_points(points.clone());
     commands.insert_resource(CurrentPath(path));
 
-    // Draw grass
-    for x in 0..util::size::WIDTH {
-        for y in 0..util::size::HEIGHT {
-            let tile = sprite(
-                0, x, y, util::z_pos::GRID,
-                Palette::G, Palette::Transparent,
-                false, 0, textures.mrmotext.clone(),
-            );
-            commands
-                .spawn(tile)
-                .insert(GridUI);
-        }
-    }
+    let mut grid = vec![vec![RoadElement::Plain; util::size::WIDTH]; util::size::HEIGHT];
+
+    let is_oob = |x: isize, y: isize| { x < 0 || y < 0 || x >= util::size::WIDTH as isize || y >= util::size::HEIGHT as isize };
 
     // Draw road
     for i in 0..points.len() - 1 {
         let (p1, p2) = (points[i], points[i + 1]);
         if p1.x == p2.x {
+            let x = p1.x as usize;
             let (y1, y2) = (p1.y as usize, p2.y as usize);
             let (y1, y2) = (min(y1, y2), max(y1, y2));
-            for y in y1 + 1..y2 {
-                draw_road_tiles(
-                    RoadElement::rand(), false,
-                    p1.x as usize, y,
-                    &mut commands, &textures.mrmotext,
-                );
+            for y in y1..=y2 {
+                if is_oob(x as isize, y as isize) { continue }
+                grid[y][x] = RoadElement::Road;
             }
         } else {
+            let y = p1.y as usize;
             let (x1, x2) = (p1.x as usize, p2.x as usize);
             let (x1, x2) = (min(x1, x2), max(x1, x2));
-            for x in x1 + 1..x2 {
-                draw_road_tiles(
-                    RoadElement::rand(), true,
-                    x, p1.y as usize,
-                    &mut commands, &textures.mrmotext,
-                );
+            for x in x1..=x2 {
+                if is_oob(x as isize, y as isize) { continue }
+                grid[y][x] = RoadElement::Road;
             }
         }
     }
-    for point in &points {
-        draw_road_tiles(RoadElement::Plain, true, point.x as usize, point.y as usize, &mut commands, &textures.mrmotext);
+
+    for y in 0..util::size::HEIGHT {
+        for x in 0..util::size::WIDTH {
+            if grid[y][x] == RoadElement::Road { continue }
+            for (dx, dy) in [
+                (0, -2),
+                (-1, -1), (0, -1), (1, -1),
+                (-2, 0), (-1, 0), (1, 0), (2, 0),
+                (-1, 1), (0, 1), (1, 1),
+                (0, 2),
+            ] {
+                if is_oob(x as isize + dx, y as isize + dy) { continue }
+                if grid[(y as isize + dy) as usize][(x as isize + dx) as usize] == RoadElement::Road {
+                    grid[y][x] = RoadElement::Rock;
+                    continue
+                }
+            }
+        }
     }
+
+    draw_road_tiles(&grid, &mut commands, &textures.mrmotext);
 }
 
-#[derive(Rand)]
+#[derive(Rand, Copy, Clone, PartialEq)]
 enum RoadElement {
-    #[weight(10)]
     Plain,
-    Crossing,
-    #[weight(2)]
-    Plant1,
-    Crack,
-    Paper,
+    Road,
+    Rock,
 }
 
 // TODO: Types of elements (4 tiles, 1 tile random pos, 1 tile repeated)
 impl RoadElement {
     /// Returns tiles for a road element with horizontal orientation
-    fn get_tiles(&self, horizontal: bool) -> [TILE; 4] {
-        match (self, horizontal) {
-            (RoadElement::Plain, _) => [
-                (0, 1, 0, 3, 16, false, 0),
-                (1, 1, 0, 3, 16, false, 0),
-                (0, 0, 0, 3, 16, false, 0),
-                (1, 0, 0, 3, 16, false, 0),
-            ],
-            (RoadElement::Crossing, true) => [
-                (0, 1, 460, 3, 0, false, 1),
-                (1, 1, 460, 3, 0, false, 1),
-                (0, 0, 460, 3, 0, false, 1),
-                (1, 0, 460, 3, 0, false, 1),
-            ],
-            (RoadElement::Crossing, false) => [
-                (0, 1, 460, 3, 0, false, 0),
-                (1, 1, 460, 3, 0, false, 0),
-                (0, 0, 460, 3, 0, false, 0),
-                (1, 0, 460, 3, 0, false, 0),
-            ],
-            (RoadElement::Plant1, _) => [
-                (0, 1, 462, 3, 7, true, 0),
-                (1, 1, 0, 3, 7, true, 0),
-                (0, 0, 0, 3, 7, false, 0),
-                (1, 0, 0, 3, 7, false, 0),
-            ],
-            (RoadElement::Crack, true) => [
-                (0, 1, 648, 3, 4, true, 0),
-                (1, 1, 647, 3, 4, true, 0),
-                (0, 0, 647, 3, 4, false, 0),
-                (1, 0, 648, 3, 4, false, 0),
-            ],
-            (RoadElement::Crack, false) => [
-                (0, 1, 647, 3, 4, false, 1),
-                (1, 1, 648, 3, 4, true, 3),
-                (0, 0, 648, 3, 4, false, 1),
-                (1, 0, 647, 3, 4, true, 3),
-            ],
-            (RoadElement::Paper, _) => [
-                (0, 1, 0, 3, 16, true, 0),
-                (1, 1, 0, 3, 16, true, 0),
-                (0, 0, 0, 3, 16, true, 0),
-                (1, 0, 109, 3, 0, true, 0),
-            ],
+    fn get_tiles(&self) -> (sprites::INDEX, sprites::BG, sprites::FG, sprites::FLIP, sprites::ROTATION) {
+        match self {
+            RoadElement::Plain | RoadElement::Road => (0, 4, 3, false, 0),
+            RoadElement::Rock => ((202 + rand::thread_rng().next_u32() % 3) as usize, 4, 3, false, 0),
         }
     }
 }
 
-fn draw_road_tiles(element: RoadElement, horizontal: bool, x: usize, y: usize, commands: &mut Commands, atlas: &Handle<TextureAtlas>) {
-    for (dx, dy, i, bg, fg, flip, rotation) in element.get_tiles(horizontal) {
-        let tile = sprite(
-            i, 2 * x + dx, 2 * y + dy, util::z_pos::ROAD,
-            bg.into(), fg.into(),
-            flip, rotation, atlas.clone(),
-        );
-        commands
-            .spawn(tile)
-            .insert(GridUI);
+fn draw_road_tiles(grid: &Vec<Vec<RoadElement>>, commands: &mut Commands, atlas: &Handle<TextureAtlas>) {
+    for y in 0..grid.len() {
+        for x in 0..grid[y].len() {
+            let (i, bg, fg, f, r) = grid[y][x].get_tiles();
+            let tile = sprite(
+                i, x, y, util::z_pos::ROAD,
+                bg.into(), fg.into(), f, r, atlas.clone(),
+            );
+            commands
+                .spawn(tile)
+                .insert(GridUI);
+        }
     }
 }
 
--- src/util.rs
+++ src/util.rs
@@ -3,16 +3,16 @@ pub mod size {
     const TILE_SIZE: usize = 8;
 
     /// Screen size in tiles
-    pub const WIDTH: usize = 40;
-    pub const HEIGHT: usize = 22;
+    pub const WIDTH: usize = 20;
+    pub const HEIGHT: usize = 12;
 
     /// Camera scale
-    pub const SCALE: f32 = 4.;
+    pub const SCALE: f32 = 8.;
 
     /// Returns world coordinates for a tile, for instance `2` -> `(2 * TILE_SIZE) as f32 `.
     pub const fn tile_to_f32(tile: usize) -> f32 { (tile * TILE_SIZE) as f32 }
 
-    pub fn path_to_f32(pos: f32) -> f32 { pos * TILE_SIZE as f32 * 2. }
+    pub fn path_to_f32(pos: f32) -> f32 { pos * TILE_SIZE as f32 }
 }
 
 pub mod z_pos {
