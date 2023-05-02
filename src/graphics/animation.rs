dfc4fe459e79e6cab6bf62d7aff3e5cc8c1edb98
yopox <yopoxdev@gmail.com>
2023-05-02 09:40:41 +0200

    animation: Fix rotating package

--- src/graphics/animation.rs
+++ src/graphics/animation.rs
@@ -1,15 +1,11 @@
 use bevy::prelude::*;
 use bevy_text_mode::TextModeTextureAtlasSprite;
 
-use crate::util;
-
 pub struct AnimationPlugin;
 
 impl Plugin for AnimationPlugin {
     fn build(&self, app: &mut App) {
         app
-            .insert_resource(AnimationTimer(util::misc::ANIMATION_INTERVAL))
-            .add_system(animate)
             .add_system(wiggle);
     }
 }
@@ -24,38 +20,6 @@ fn rotate(sprite: &TextModeTextureAtlasSprite, rotation: u8) -> TextModeTextureA
     let mut new_sprite = sprite.clone();
     new_sprite.rotation = (new_sprite.rotation + rotation) % 4;
     return new_sprite;
-}
-
-fn animation(sprite: &TextModeTextureAtlasSprite) -> Option<TextModeTextureAtlasSprite> {
-    match sprite.index {
-        268 => Some(flip(sprite)),
-        367 => Some(flip(sprite)),
-        397 => Some(rotate(sprite, 1)),
-        619 => Some(flip(&rotate(sprite, 2))),
-        _ => None,
-    }
-}
-
-#[derive(Resource)]
-struct AnimationTimer(usize);
-
-#[derive(Component)]
-pub struct NoAnimation;
-
-fn animate(
-    mut timer: ResMut<AnimationTimer>,
-    mut tiles: Query<&mut TextModeTextureAtlasSprite, Without<NoAnimation>>,
-) {
-    timer.0 -= 1;
-    if timer.0 <= 0 {
-        timer.0 = util::misc::ANIMATION_INTERVAL;
-
-        for mut sprite in tiles.iter_mut() {
-            let Some(new_sprite) = animation(&sprite) else { continue };
-            sprite.flip_x = new_sprite.flip_x;
-            sprite.rotation = new_sprite.rotation;
-        }
-    }
 }
 
 #[derive(Component)]
